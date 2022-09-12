101e7205f323eefdc096a52f01682a4528bc71ee
kornelharm <kornelharm@gmail.com>
2022-09-11 19:41:37 -0500

    Modified sheets to utilize service accounts

--- sheets.py
+++ sheets.py
@@ -1,49 +1,41 @@
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import os.path
+from google.oauth2 import service_account
+import os
+import json
 
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-def instantiate(tokenPath="local/token.json", credPath="local/credentials.json"):
+def instantiate(credPath="local/credentials.json"):
 	"""
 	Constructs and authenticates an instance of the Google Sheets API service
 	
 	Arguments
-		`tokenPath="local/token.json` (str) : Filepath to token.json
-		`credPath="local/credentials.json` (str) : Filepath to credentials.json
+		`credPath="local/credentials.json` (str) : Filepath to service account credentials
 
 	Returns:
 		Sheets API instance
 	"""
+	os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credPath
 	creds = None
-	if os.path.exists(tokenPath):
-		creds = Credentials.from_authorized_user_file(tokenPath, SCOPES)
-	# If there are no (valid) credentials available, let the user log in.
-	if not creds or not creds.valid:
-		if creds and creds.expired and creds.refresh_token:
-			creds.refresh(Request())
-		else:
-			flow = InstalledAppFlow.from_client_secrets_file(credPath, SCOPES)
-			creds = flow.run_local_server(port=0)
-		# Save the credentials for the next run
-		with open(tokenPath, 'w') as token:
-			token.write(creds.to_json())	
+	if os.path.exists(credPath):
+		accountInfo = json.load(open(credPath))
+		creds = service_account.Credentials.from_service_account_info(accountInfo, scopes=SCOPES)
+	else:
+		raise Exception("Credentials file could not be found")
 	try:
 		service = build('sheets', 'v4', credentials=creds)
 		return service.spreadsheets()	
 	except HttpError as err:
 		print(err)
 
-def getSpreadsheet(path="local/sheetID.txt"):
+def getSpreadsheet(path):
 	"""
 	Finds the locally stored spreadsheet ID to interact with
 
 	Arguments
-		`path="local/sheetID.txt"` (str) : Path to file with the ID
+		`path` (str) : Path to file with the ID
 
 	Returns:
 		`str` : locally stored spreadsheet ID
@@ -78,7 +70,6 @@ def writeTopLeft(service, sheet, spreadsheetID, content):
 		`sheet` (str) : The name of the sheet to be cleared
 		`spreadsheetID` (str) : The ID of the spreadsheet
 		`content` (List[List]) : 2-D List of JSON serializable content
-
 	"""
 	_body = {"values" : content}
 	service.values().update(
@@ -86,6 +77,26 @@ def writeTopLeft(service, sheet, spreadsheetID, content):
 				range=f"{sheet}!A1",
 				valueInputOption="RAW",
 				body=_body).execute()
+
+def readColumn(service, sheet, spreadsheetID, column, ignoreHeader):
+	"""
+	Reads content in the column of the specified sheet on the given spreadsheet. 
+
+	Arguments
+		`service` (?) : The Sheets API service
+		`sheet` (str) : The name of the sheet to be cleared
+		`spreadsheetID` (str) : The ID of the spreadsheet
+		`column` (str) : Letter indicating desired column
+		`ignoreHeader` (bool) : Determines whether first row is read
+	"""
+	columnRange = f"{sheet}!{column}"
+	if(ignoreHeader):
+		columnRange = f"{sheet}!{column}2:{column}"
+	resp = service.values().get(
+				spreadsheetId=spreadsheetID,
+				range=columnRange,
+				valueRenderOption="FORMATTED_VALUE").execute()
+	return resp["values"]
 
 
 
