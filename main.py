379689f945c72a1c57efb1b6dc82ed6ae81ea8df
kornelharm <kornelharm@gmail.com>
2022-09-11 19:42:09 -0500

    Implemented sheets input and date filtering

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+from operator import truediv
 import os
 import utils
 import scraper
 import sheets
-from googleapiclient.errors import HttpError
 
 if __name__ == "__main__":
 
@@ -14,20 +14,33 @@ if __name__ == "__main__":
 	
 	try:		
 		sheetsAPI = sheets.instantiate()
-		spreadsheetID = sheets.getSpreadsheet()
-		sheets.clearSheet(sheetsAPI, "Schedule", spreadsheetID)
-		matches = scraper.get_team_matches(driver, teamID)
+		inSheet = sheets.getSpreadsheet("local/inSheet.txt")
+		outputSheet = sheets.getSpreadsheet("local/outSheet.txt")
+		teams = sheets.readColumn(sheetsAPI, "Tracker", inSheet, "A", True)
+		sheets.clearSheet(sheetsAPI, "Schedule", outputSheet)
 		content = []
-		for match in matches:
-			_match = []
-			_match.append((match.team1.name if match.team1 != None else ""))
-			_match.append((match.team2.name if match.team2 != None else ""))
-			_match.append((match.map if match.map != None else ""))
-			_match.append((match.date if match.date != None else ""))
-			content.append(_match)
+		for team in teams:
+			_team = team[0]
+			print(f"Working on {_team}")
+			_matches = scraper.get_team_matches(driver, _team)
+			print(f"Retrieved schedule for {_team}")
+			for match in _matches:
+				_date = (match.date if match.date != None else "")
+				hoursLeft = utils.hoursUntil(utils.strGMTtoStruct(_date))
+				if((hoursLeft > 0) and (hoursLeft < 48)):
+					print(hoursLeft)
+					_match = []
+					_match.append((match.team1.name if match.team1 != None else ""))
+					_match.append((match.team2.name if match.team2 != None else ""))
+					_match.append((match.map if match.map != None else ""))
+					_match.append(_date)
+					content.append(_match)
+				else:
+					continue
 		
-		sheets.writeTopLeft(sheetsAPI, "Schedule", spreadsheetID, content)
-
+		sheets.writeTopLeft(sheetsAPI, "Schedule", outputSheet, content)
+	except Exception as e:
+		print(e)
 
 	finally:
 		driver.quit()
